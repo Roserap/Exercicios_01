1eecae496d220378bafa9b0b984f70784c23b739
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-14 21:01:27 -0300

    Update L2_exercicio1.py

--- Lista2/L2_exercicio1.py
+++ Lista2/L2_exercicio1.py
@@ -1,1 +1,11 @@
+"""
+1. Par ou Ímpar?
+Escreva um programa que solicite um número inteiro ao usuário e informe se ele é par ou
+ímpar.
+"""
 
+numero = int(input("Digite um numero "))
+if numero % 2 == 0:
+    print(f"O numero{numero} é par.")
+else:
+    print(f"O numero{numero} é impar.")
