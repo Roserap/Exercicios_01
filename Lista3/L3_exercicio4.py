6b22ecb87105dd19fd0766105165e518524daba5
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:22:50 -0300

    Update L3_exercicio4.py

--- Lista3/L3_exercicio4.py
+++ Lista3/L3_exercicio4.py
@@ -1,1 +1,18 @@
+""""4. Números Ímpares em um Intervalo
+Peça ao usuário dois números inteiros, representando um intervalo A,B. O programa deve
+exibir todos os números ímpares dentro desse intervalo (inclusive os limites, se forem
+ímpares)."
+"""
+a = int(input("Digite o primeiro número inteiro (A): "))
+b = int(input("Digite o segundo número inteiro (B): "))
 
+
+if a > b:
+    a, b = b, a
+
+
+print(f"Números ímpares no intervalo de {a} a {b}:")
+
+for numero in range(a, b + 1):
+    if numero % 2 != 0:
+        print(numero)
