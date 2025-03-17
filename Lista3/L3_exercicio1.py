7bad5aa2ae13d0e735e3ea59b4852fda762e0f74
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:21:23 -0300

    Update L3_exercicio1.py

--- Lista3/L3_exercicio1.py
+++ Lista3/L3_exercicio1.py
@@ -1,1 +1,19 @@
+"""
+1) Contagem Progressiva
 
+Escreva um programa que peça um número inteiro positivo ao usuário e exiba todos os
+números de 1 até esse número, um por linha.
+"""
+
+try:
+    numero = int(input("Digite um número inteiro positivo: "))
+
+    
+    if numero > 0:
+        
+        for i in range(1, numero + 1):
+            print(i)
+    else:
+        print("Por favor, digite um número inteiro positivo.")
+except ValueError:
+    print("Entrada inválida! Por favor, digite um número inteiro.")
