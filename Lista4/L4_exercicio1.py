6ab581bbd17aa2b3ea4cb15a1d3c6acb624152c1
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:57:57 -0300

    Update L4_exercicio1.py

--- Lista4/L4_exercicio1.py
+++ Lista4/L4_exercicio1.py
@@ -1,1 +1,15 @@
+"""1. Soma dos Elementos de uma Lista
+Escreva um programa que solicite ao usuário uma lista de números inteiros e
+calcule a soma de todos os elementos da lista."""
 
+
+
+entrada = input("Digite uma lista de números inteiros, separados por espaço: ")
+
+numeros = []
+for num in entrada.split():
+    numeros.append(int(num))
+
+soma = sum(numeros)
+
+print("A soma dos elementos da lista é:", soma)
