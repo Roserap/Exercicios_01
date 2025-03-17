7a1a2bbfcd9c07b2e7f38251577d04c3cfd47389
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:58:38 -0300

    Update L4_exercicio3.py

--- Lista4/L4_exercicio3.py
+++ Lista4/L4_exercicio3.py
@@ -1,1 +1,15 @@
+"""3. Remover Duplicatas
+Escreva um programa que leia uma lista de números e remova os valores
+duplicados, mantendo a ordem original."""
 
+entrada = input("Digite uma lista de números, separados por espaço: ")
+
+numeros = [int(num) for num in entrada.split()]
+
+numeros_unicos = []
+
+for num in numeros:
+    if num not in numeros_unicos:
+        numeros_unicos.append(num)
+
+print("Lista sem duplicatas:", numeros_unicos)
