09be6dfaaca571eb69b9320abf7f98616d06d53e
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:59:23 -0300

    Update L4_exercicio5.py

--- Lista4/L4_exercicio5.py
+++ Lista4/L4_exercicio5.py
@@ -1,1 +1,14 @@
+"""5. Contar Ocorrências de um Elemento
+Escreva um programa que peça ao usuário para inserir uma lista de números e um
+número específico. O programa deve contar quantas vezes esse número aparece
+na lista."""
 
+entrada = input("Digite uma lista de números, separados por espaço: ")
+
+numeros = [int(num) for num in entrada.split()]
+
+numero_especifico = int(input("Digite o número que deseja contar: "))
+
+ocorrencias = numeros.count(numero_especifico)
+
+print(f"O número {numero_especifico} aparece {ocorrencias} vez(es) na lista.")
