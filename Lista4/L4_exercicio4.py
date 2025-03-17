aca1e565087c14bc823430086e9170164c42629f
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:59:00 -0300

    Update L4_exercicio4.py

--- Lista4/L4_exercicio4.py
+++ Lista4/L4_exercicio4.py
@@ -1,1 +1,11 @@
+"""4. Inverter a Lista
+Escreva um programa que leia uma lista de palavras e exiba essa lista na ordem
+inversa."""
 
+entrada = input("Digite uma lista de palavras, separadas por espaço: ")
+
+palavras = entrada.split()
+
+palavras_invertidas = palavras[::-1]
+
+print("Lista de palavras na ordem inversa:", palavras_invertidas)
