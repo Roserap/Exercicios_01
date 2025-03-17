7ba48e7cc8b2ea43b9ebd41f1bcc890ff26e3ac6
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:21:55 -0300

    Update L3_exercicio2.py

--- Lista3/L3_exercicio2.py
+++ Lista3/L3_exercicio2.py
@@ -1,1 +1,22 @@
+"""
+2. Soma de Números Positivos
+Faça um programa que solicite números ao usuário até que ele digite um número negativo.
+Quando isso acontecer, o programa deve exibir a soma de todos os números positivos
+digitados e encerrar."
+"""
 
+soma = 0
+
+while True:
+    
+    numero = float(input("Digite um número (negativo para encerrar): "))
+    
+    
+    if numero < 0:
+        break
+    
+    
+    soma += numero
+
+
+print("A soma dos números positivos digitados é:", soma)
