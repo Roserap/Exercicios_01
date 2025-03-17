ebd5b1dce7bbc19035d86fa5653b9f1837b9a0ad
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:23:14 -0300

    Update L3_exercicio5.py

--- Lista3/L3_exercicio5.py
+++ Lista3/L3_exercicio5.py
@@ -1,1 +1,22 @@
+""""5. Sequência de Fibonacci
+Escreva um programa que peça ao usuário um número N e exiba os N primeiros termos da
+Sequência de Fibonacci.
+Obs: A sequência de Fibonacci começa com 0 e 1, e cada termo seguinte é a soma dos dois
+anteriores:"""
 
+n = int(input("Digite um número inteiro positivo (N): "))
+
+if n <= 0:
+    print("Por favor, digite um número inteiro positivo.")
+else:
+    
+    fibonacci = [0, 1]
+
+    
+    for i in range(2, n):
+        proximo_termo = fibonacci[i - 1] + fibonacci[i - 2]
+        fibonacci.append(proximo_termo)
+
+    
+    print(f"Os {n} primeiros termos da sequência de Fibonacci são:")
+    print(fibonacci[:n])
