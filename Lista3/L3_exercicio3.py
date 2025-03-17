5306134cc530df4382abe0ee50eddd4526c2b030
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-17 13:22:24 -0300

    Update L3_exercicio3.py

--- Lista3/L3_exercicio3.py
+++ Lista3/L3_exercicio3.py
@@ -1,1 +1,11 @@
+"""""
+"3. Tabuada de um Número
+Peça um número inteiro ao usuário e exiba a tabuada desse número de 1 a 10."
+"""
 
+numero = int(input("Digite um número inteiro: "))
+
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
