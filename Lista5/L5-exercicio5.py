04857e49ac091d551c92ccaef3afff844af4983d
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-29 11:29:11 -0300

    Update L5-exercicio5.py

--- Lista5/L5-exercicio5.py
+++ Lista5/L5-exercicio5.py
@@ -1,1 +1,17 @@
+"""5. Conversão de Temperatura
+Crie uma função que converta temperaturas de Celsius para Fahrenheit. No
+programa principal, solicite uma temperatura ao usuário em graus Celsius e utilize
+a função para exibir o valor correspondente em Fahrenheit."""
 
+def celsius_para_fahrenheit(celsius):
+    
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+if __name__ == "__main__":
+    
+    temperatura_celsius = float(input("Digite a temperatura em graus Celsius: "))
+    
+    temperatura_fahrenheit = celsius_para_fahrenheit(temperatura_celsius)
+    
+    print(f"A temperatura em Fahrenheit é: {temperatura_fahrenheit:.2f} °F")
