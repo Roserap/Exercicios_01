8678aa5a342c33b07b326a4fbf36bb5e2e9a9c2e
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-29 11:28:44 -0300

    Update L5-exercicio4.py

--- Lista5/L5-exercicio4.py
+++ Lista5/L5-exercicio4.py
@@ -1,1 +1,20 @@
+"""4. Contador de Vogais em uma Palavra
+Crie uma função que receba uma palavra como parâmetro e retorne a quantidade
+de vogais presentes nela. No programa principal, solicite uma palavra ao usuário e
+utilize a função para exibir o número de vogais."""
 
+def contar_vogais(palavra):
+    
+    vogais = "aeiouAEIOU"
+    contador = 0
+    
+    for letra in palavra:
+        if letra in vogais:
+            contador += 1
+
+    return contador
+
+if __name__ == "__main__":
+    palavra = input("Digite uma palavra: ")
+    quantidade_vogais = contar_vogais(palavra)
+    print(f"A quantidade de vogais na palavra '{palavra}' é: {quantidade_vogais}")
