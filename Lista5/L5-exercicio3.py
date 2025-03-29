6f9f6450311ca7ec2eb75991844e02f0f9816e13
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-29 11:28:18 -0300

    Update L5-exercicio3.py

--- Lista5/L5-exercicio3.py
+++ Lista5/L5-exercicio3.py
@@ -1,1 +1,29 @@
+"""3. Cálculo da Média de uma Lista
+Crie uma função que receba uma lista de números e retorne a média dos valores.
+No programa principal, peça ao usuário para inserir os números e exiba a média
+utilizando a função criada."""
 
+def calcular_media(lista):
+    
+    if len(lista) == 0:
+        return 0
+    
+    soma = sum(lista)
+    
+    media = soma / len(lista)
+    return media
+
+if __name__ == "__main__":
+    numeros = []
+    while True:
+        entrada = input("Digite um número (ou 'sair' para finalizar): ")
+        if entrada.lower() == 'sair':
+            break
+        try:
+            numero = float(entrada)
+            numeros.append(numero)
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
+    media = calcular_media(numeros)
+    print(f"A média dos números inseridos é: {media}")
