fe34ef211a40bdab906d49e34aeb4f838fd73e10
Roserap <125290089+Roserap@users.noreply.github.com>
2025-03-29 11:27:47 -0300

    Update L5-exercicio2.py

--- Lista5/L5-exercicio2.py
+++ Lista5/L5-exercicio2.py
@@ -1,1 +1,21 @@
+"""2. Verificação de Número Primo
+Crie uma função que receba um número inteiro e retorne True se for primo e False
+caso contrário. No programa principal, solicite um número ao usuário e utilize a
+função para verificar se ele é primo."""
 
+def eprimo(n):
+    
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    numero = int(input("Informe um número inteiro para verificar se é primo: "))
+    if eprimo(numero):
+        print(f"{numero} é um número primo.")
+    else:
+        print(f"{numero} não é um número primo.")
