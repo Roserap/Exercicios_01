d290e51bede0b32bc2a385af2d6e60d6553dd9f4
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:41:11 -0300

    Update exercicio - 08.py

--- modulo2/exercicio - 08.py
+++ modulo2/exercicio - 08.py
@@ -1,1 +1,25 @@
 
+class Livro:
+    def __init__(self, titulo, autor, paginas):
+        
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        
+        return f"Título: {self.titulo}\nAutor: {self.autor}\nPáginas: {self.paginas}"
+
+    def __len__(self):
+        
+        return self.paginas
+
+
+livro1 = Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1216)
+
+
+print("Detalhes do Livro:")
+print(livro1)
+
+
+print(f"Número de páginas: {len(livro1)}")
