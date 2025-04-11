c81a65732741c2448561bbfc7cae1e9962a3233c
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:36:05 -0300

    Update exercicio - 02.py

--- modulo2/exercicio - 02.py
+++ modulo2/exercicio - 02.py
@@ -1,1 +1,9 @@
+import platform
 
+nome_sistema = platform.system()
+versao_sistema = platform.version()
+arquitetura_processador = platform.architecture()[0]
+
+print(f"Nome do Sistema Operacional: {nome_sistema}")
+print(f"Versão do Sistema: {versao_sistema}")
+print(f"Arquitetura do Processador: {arquitetura_processador}")
