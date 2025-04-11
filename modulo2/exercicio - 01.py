858da9bd43a9f8ded9d4cde1e80083f949f9ad2c
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:35:16 -0300

    Update exercicio - 01.py

--- modulo2/exercicio - 01.py
+++ modulo2/exercicio - 01.py
@@ -1,1 +1,10 @@
+import math
+import random
 
+numero = float(input("Informe um número para calcular a raiz quadrada: "))
+
+raiz_quadrada = math.sqrt(numero)
+print(f"A raiz quadrada de {numero} é {raiz_quadrada:.2f}")
+
+numero_aleatorio = random.randint(1, 100)
+print(f"O número aleatório gerado entre 1 e 100 é: {numero_aleatorio}")
