1dcb8de011e42bfe1a66c064b1c8e314b592c51e
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:42:48 -0300

    Update exercicio - 11.py

--- modulo2/exercicio - 11.py
+++ modulo2/exercicio - 11.py
@@ -1,1 +1,27 @@
 
+class Contador:
+    def __init__(self, n):
+        
+        self.n = n
+        self.atual = 1
+
+    def __iter__(self):
+        
+        return self
+
+    def __next__(self):
+        
+        if self.atual <= self.n:
+            numero = self.atual
+            self.atual += 1
+            return numero
+        else:
+            raise StopIteration
+
+n = int(input("Digite um número: "))
+
+contador = Contador(n)
+
+print(f"Contando de 1 até {n}:")
+for numero in contador:
+    print(numero)
