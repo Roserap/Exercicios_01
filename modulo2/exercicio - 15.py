24c3c480152463cb29145801ee98d6795283fb6e
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:45:25 -0300

    Update exercicio - 15.py

--- modulo2/exercicio - 15.py
+++ modulo2/exercicio - 15.py
@@ -1,1 +1,19 @@
+import os
+import datetime
+import calendar
+import time
 
+diretorio_atual = os.getcwd()
+print(f"Diretório atual: {diretorio_atual}")
+
+data_hora_atual = datetime.datetime.now()
+print(f"Data e hora atuais: {data_hora_atual}")
+
+ano_atual = data_hora_atual.year
+mes_atual = data_hora_atual.month
+calendario = calendar.month(ano_atual, mes_atual)
+print(f"\nCalendário do mês atual:\n{calendario}")
+
+print("Fazendo uma pausa de 3 segundos...")
+time.sleep(3)
+print("Pausa concluída!")
