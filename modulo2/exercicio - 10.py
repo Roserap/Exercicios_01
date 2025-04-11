23080bb2943bc6a4dbce6c1e3e70b6cf7db4983d
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:42:18 -0300

    Update exercicio - 10.py

--- modulo2/exercicio - 10.py
+++ modulo2/exercicio - 10.py
@@ -1,1 +1,12 @@
 
+def contar_pares(n):
+    
+    for i in range(0, n + 1, 2):
+        yield i
+
+
+n = int(input("Digite um número: "))
+
+print(f"Números pares até {n}:")
+for par in contar_pares(n):
+    print(par)
