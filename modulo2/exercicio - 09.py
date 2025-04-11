fdc16f7ef8be77eadf725cc8fa2559156dc919a8
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:41:45 -0300

    Update exercicio - 09.py

--- modulo2/exercicio - 09.py
+++ modulo2/exercicio - 09.py
@@ -1,1 +1,21 @@
 
+class Animal:
+    def fazer_som(self):
+        
+        pass
+
+class Cachorro(Animal):
+    def fazer_som(self):
+        
+        print("O cachorro faz: Au Au!")
+
+class Gato(Animal):
+    def fazer_som(self):
+        
+        print("O gato faz: Miau!")
+
+cachorro = Cachorro()
+cachorro.fazer_som()
+
+gato = Gato()
+gato.fazer_som()
