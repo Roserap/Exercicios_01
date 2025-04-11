9a4710ba597b761974362114e7e522a1a0b17b97
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:43:26 -0300

    Update exercicio - 12.py

--- modulo2/exercicio - 12.py
+++ modulo2/exercicio - 12.py
@@ -1,1 +1,17 @@
 
+def multiplicador(fator):
+    
+    def multiplicar(numero):
+        return numero * fator
+    return multiplicar
+
+
+fator = float(input("Digite um fator para multiplicação: "))
+
+multiplica_por_fator = multiplicador(fator)
+
+numero = float(input("Digite um número para multiplicar: "))
+
+resultado = multiplica_por_fator(numero)
+
+print(f"O resultado de {numero} multiplicado por {fator} é: {resultado}")
