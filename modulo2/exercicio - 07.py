d1b7e48c194ca0bf2863414b045f7e41ac443605
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:40:25 -0300

    Update exercicio - 07.py

--- modulo2/exercicio - 07.py
+++ modulo2/exercicio - 07.py
@@ -1,1 +1,24 @@
 
+class Carro:
+    def __init__(self, marca, modelo, ano):
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+    def exibir_detalhes(self):
+        
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print(f"Ano: {self.ano}")
+
+
+carro1 = Carro("Toyota", "Corolla", 2020)
+
+carro2 = Carro("Honda", "Civic", 2021)
+
+print("Detalhes do Carro 1:")
+carro1.exibir_detalhes()
+
+print("\nDetalhes do Carro 2:")
+carro2.exibir_detalhes()
