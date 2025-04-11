6e7a5209d089a3800fd73d3bb0437a32de439767
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:38:42 -0300

    Update exercicio - 05.py

--- modulo2/exercicio - 05.py
+++ modulo2/exercicio - 05.py
@@ -1,1 +1,14 @@
 
+entrada = input("Digite uma lista de palavras separadas por espaço: ")
+
+lista_palavras = entrada.split()
+
+lista_ordenada = sorted(lista_palavras)
+
+numero_palavras = len(lista_palavras)
+
+palavras_maiusculas = [palavra.upper() for palavra in lista_palavras]
+
+print("Lista ordenada alfabeticamente:", lista_ordenada)
+print("Número total de palavras:", numero_palavras)
+print("Palavras em maiúsculas:", palavras_maiusculas)
