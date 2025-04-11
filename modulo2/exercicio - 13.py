de72bd3a7975993d6e138bff7eb000924ca18be3
Roserap <125290089+Roserap@users.noreply.github.com>
2025-04-11 10:44:00 -0300

    Update exercicio - 13.py

--- modulo2/exercicio - 13.py
+++ modulo2/exercicio - 13.py
@@ -1,1 +1,21 @@
 
+nome_arquivo = 'dados.txt'
+
+try:
+    
+    with open(nome_arquivo, 'r') as arquivo:
+        
+        linhas = arquivo.readlines()
+        
+        numero_de_linhas = len(linhas)
+        
+        print(f"O arquivo '{nome_arquivo}' possui {numero_de_linhas} linhas.\n")
+        
+        print("Conteúdo do arquivo:")
+        for linha in linhas:
+            print(linha.strip())
+
+except FileNotFoundError:
+    print(f"Erro: O arquivo '{nome_arquivo}' não foi encontrado.")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
